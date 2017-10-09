40ed8c5041800eb62147db43429f77d3d33afac3
Michael Stathers <mstathers@outlook.com>
2017-10-09 16:55:45 -0700

    add pure.css and start templating pages

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-from flask import Flask
-#import os
-#import socket
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
